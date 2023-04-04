f80b2106e4f5c8e43a23d8b09518220af64820fa
Dmitriy <merk.dima15@gmail.com>
2023-04-04 15:53:07 +0300

    Zadacha47

---
+++ Zadacha47.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TwoDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 3;
+            int n = 4;
+            double[,] array = new double[m, n];
+            Random random = new Random();
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = random.NextDouble() * (random.Next(0, 2) == 0 ? 1 : -1);
+                    Console.Write($"{array[i, j]:F2} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
