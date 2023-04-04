d8571b318ae232f01285fe874935d283e363598e
Dmitriy <merk.dima15@gmail.com>
2023-04-04 15:57:23 +0300

    Zadacha52

---
+++ Zadacha52.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ArrayColumnAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[,]
+            {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 }
+            };
+
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+
+            for (int j = 0; j < columns; j++)
+            {
+                int columnSum = 0;
+
+                for (int i = 0; i < rows; i++)
+                {
+                    columnSum += array[i, j];
+                }
+
+                double columnAverage = (double)columnSum / rows;
+                Console.WriteLine($"Среднее арифметическое элементов в столбце {j}: {columnAverage}");
+            }
+        }
+    }
+}
